fd6922930ca3355ee1db39e315d8999ccb322254
thanasispro <protopapas.dim.ath@outlook.com>
2020-05-08 15:49:27 +0300

    basic route and more firebase added

--- egosuisunique-ui/src/App.js
+++ egosuisunique-ui/src/App.js
@@ -3,18 +3,48 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Login from './components/login.component'
+import { Route, Switch } from 'react-router-dom';
+import Login from './components/login/login.component'
+import Welcome from './components/welcome/welcome.component'
+import { auth } from './firebase/firebase.utils';
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql"
 });
 
-function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Login></Login>
-    </ApolloProvider>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribe = null;
+
+  componentDidMount() {
+   this.unsubscribe =  auth.onAuthStateChanged(user => {
+      this.setState({ currentUser : user})
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <Switch>
+         <Route exact path='/' component={ Login }></Route>
+         <Route exact path='/welcome' component= { Welcome}></Route>
+        </Switch>
+      </ApolloProvider>
+    );
+  }
+ 
 }
 
 export default App;
